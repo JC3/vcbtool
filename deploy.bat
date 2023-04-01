7eeb36ef75184059d8b45a08b6f1e9b0e20a3d24
Jason C <jason.cipriani@gmail.com>
2023-04-01 12:07:56 -0400

    windows deploy script

---
+++ deploy.bat
@@ -0,0 +1,6 @@
+cd vcbtool-win64
+cp ..\release\vcbtool.exe .
+cp ..\font_3x5.png .
+cp ..\font_5x7.png .
+windeployqt vcbtool.exe --no-quick-import --no-system-d3d-compiler --no-translations --no-virtualkeyboard --no-opengl-sw --no-svg
+
