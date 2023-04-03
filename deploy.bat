b9ed0f9bc13b48beea61515fdcba14b4190df3a3
Jason C <jason.cipriani@gmail.com>
2023-04-03 07:35:43 -0400

    topaz sans font
    renamed fixedsys font image

--- deploy.bat
+++ deploy.bat
@@ -3,7 +3,9 @@ cp ..\release\vcbtool.exe .
 cp ..\font_3x5.png .
 cp ..\font_4x5.png .
 cp ..\font_5x7.png .
-cp ..\font_fixedsys12.png .
+cp ..\font_fixedsys.png .
+cp ..\font_topaz.png .
+cp ..\font_topaz_sans.png .
 cp ..\fonts.json .
 windeployqt vcbtool.exe --no-quick-import --no-system-d3d-compiler --no-translations --no-virtualkeyboard --no-opengl-sw --no-svg
 
